3638e6a4a34cfb2f1249c795c426146717a34e1b
JadeYew <31779718+JadeYew@users.noreply.github.com>
2018-10-05 13:41:23 +0200

    Initial commit

---
+++ src/test/java/guru/springframework/spring5webbapp/Spring5webbappApplicationTests.java
@@ -0,0 +1,16 @@
+package guru.springframework.spring5webbapp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Spring5webbappApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
